00759b030f64132917cdd6364ca12e90c3bfbe6c
Alejandro Gracia <99836064+ShiroPeDeFresa@users.noreply.github.com>
2023-01-16 10:56:17 +0100

    No se que esta pasando pero bueno
    solo he cambiado el template, el higlight del codigo me sale en naranja cuando deberia ser blanco excepto los elementos entre corchetes pero bueno la  vida supongo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`<h1 align="center"> Hola esto es {{title}}</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
